7c4882da87f7659f0509fc22f278c446a715aa4b
AbdelhakRazi <razi.contact3@gmail.com>
2025-02-10 20:11:06 +0100

    fixed file logging issue

--- src/logging/trace.h
+++ src/logging/trace.h
@@ -11,7 +11,7 @@
 #include <string>
 
 namespace {
-     void format_str(const char *level, const char *fmt, ...) // macro will expand to: trace::format_str, which is not accessible so that's why we can't put it in separate
+     void format_str(const char* file_name, int line, const char *level, const char *fmt...) // macro will expand to: trace::format_str, which is not accessible so that's why we can't put it in separate
     {
         std::ofstream ofs("log.txt", std::ofstream::out | std::ofstream::app);
         std::time_t t = std::time(nullptr);
@@ -25,20 +25,21 @@ namespace {
         va_start(args1, fmt);
         std::vsnprintf(buf.data(), buf.size(), fmt, args1); // format va list to a buffer of strings
         va_end(args1);
-        std::cout << level << __FILE_NAME__ << " line." << __LINE__ << " " << time_buf << buf.data() << "\n";
-        ofs << level << __FILE_NAME__ << " line." << __LINE__ << " " << time_buf << buf.data() << "\n";
+        std::cout << level << file_name << "." << line << " " << time_buf << buf.data() << "\n";
+        ofs << level << file_name << "." << line << " " << time_buf << buf.data() << "\n";
         ofs.close();
     }
 }
+// pass file and line explicitly, otherwise it would take the file as trace.h
 namespace trace {
 #ifdef DEBUG
-    #define TRACE_DEBUG(...) format_str("[DEBUG] ", __VA_ARGS__)
+    #define TRACE_DEBUG(...) format_str(__FILE_NAME__, __LINE__, "[DEBUG] ", __VA_ARGS__)
 #else
     #define TRACE_DEBUG(...) // No-op when DEBUG is not defined
 #endif // DEBUG
 
-#define TRACE_INFO(...) format_str("[INFO] ", __VA_ARGS__)
-#define TRACE_ERROR(...) format_str("[ERROR] ", __VA_ARGS__)
+#define TRACE_INFO(...) format_str(__FILE_NAME__, __LINE__,  "[INFO] ",  __VA_ARGS__)
+#define TRACE_ERROR(...) format_str(__FILE_NAME__, __LINE__, "[ERROR] ", __VA_ARGS__)
     // vsprintf has been depcrecated due to security issues.
     // use vsnprintf
 } // namespace trace
